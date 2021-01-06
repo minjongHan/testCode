8f73b14611b86a19793784e6989d3b2a6c657530
mojjatop <74394404+mojjatop@users.noreply.github.com>
2021-01-06 23:49:08 +0900

    Add files via upload

---
+++ Practics.py
@@ -0,0 +1,32 @@
+# print(3)
+# print("안녕하세요")
+# print(0.13)
+# #name="강아지"
+# age=10
+# print("우리집 "+name+"는 나이가 "+str(age)+"살 입니다.") 
+# print("우리집 ",name,"는 나이가 ",age,"살 입니다.") 
+# print(10 < 5)
+
+#애완동물을 소개해 주세요~
+animal = "강아지"
+name = "연탕이"
+age = 4
+hobby = "산책"
+is_adult = age >= 3
+print("우리집 "+animal+"의 이름은 "+name+"이에요")
+print(name+"는 "+str(age)+"이며, "+hobby+"을 아주 좋아해요")
+print(name+"는 어른일까요? "+ str(is_adult))
+
+stations = ["사당","신도림","인천공항"]
+
+for station in stations:
+    print(station + "행 열차가 들어오고 있습니다.")
+
+print(2**3)
+print(5%3)
+print(10%3)
+print(5//3)
+print(5/3)
+
+print(10 > 3)
+print(4 >= 7)
